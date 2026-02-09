96c16ccfb9a8a3c2f73a008c7c0d08ade824aebf
Diego Román Pose <124998332+diegor2706@users.noreply.github.com>
2026-02-09 13:00:41 +0100

    Ejercicios 2 y 3
    Modificaciones del trabajo de hace 2 años

---
+++ ejercicio2.jl
@@ -0,0 +1,307 @@
+using Statistics
+using Flux
+using Flux.Losses
+using Random
+
+#ENTRENAMIENTO RR.NN.AA
+
+function oneHotEncoding(feature::AbstractArray{<:Any,1}, classes::AbstractArray{<:Any,1})
+    classes = sort(classes)
+    numClasses = length(classes)
+    
+    if numClasses <= 2
+        return reshape(feature .== classes[1], :, 1)
+    else
+        oneHot = BitArray{2}(undef, length(feature), numClasses)
+        for i = 1:numClasses
+            oneHot[:, i] .= (feature .== classes[i])
+        end
+        return oneHot
+    end
+end
+
+oneHotEncoding(feature::AbstractArray{<:Any,1}) = oneHotEncoding(feature, unique(feature))
+
+oneHotEncoding(feature::AbstractArray{Bool,1}) = reshape(feature, :, 1)
+
+function calculateMinMaxNormalizationParameters(dataset::AbstractArray{<:Real,2})
+    minValues = minimum(dataset, dims=1);
+    maxValues = maximum(dataset, dims=1);
+#    return (hcat(minValues[1], maxValues[1]), hcat(minValues[2], maxValues[2]))
+    return (minValues,maxValues)
+end
+    
+function calculateZeroMeanNormalizationParameters(dataset::AbstractArray{<:Real,2})
+    avgValues = mean(dataset, dims=1);
+    stdValues = std(dataset, dims=1);
+#    return (hcat(avgValues[1], stdValues[1]), hcat(avgValues[2], stdValues[2]))
+    return (avgValues,stdValues)
+end
+
+function normalizeMinMax!(dataset::AbstractArray{<:Real,2}, normalizationParameters::NTuple{2, AbstractArray{<:Real,2}})
+    dataset .-= normalizationParameters[1];
+    dataset ./= (normalizationParameters[2] .- normalizationParameters[1]);
+    dataset[:, vec(normalizationParameters[1].==normalizationParameters[2])] .= 0;
+end
+
+function normalizeMinMax!(dataset::AbstractArray{<:Real,2})
+    mins_maxs=calculateMinMaxNormalizationParameters(dataset);
+    normalizeMinMax!(dataset, mins_maxs)
+end
+
+function normalizeMinMax( dataset::AbstractArray{<:Real,2}, normalizationParameters::NTuple{2, AbstractArray{<:Real,2}})
+    dataset2=copy(dataset);
+    normalizeMinMax!(dataset2, normalizationParameters);
+    return dataset2;
+end
+
+function normalizeMinMax( dataset::AbstractArray{<:Real,2}) 
+    dataset2=copy(dataset);
+    normalizeMinMax!(dataset2);
+    return dataset2;
+end
+
+function normalizeZeroMean!(dataset::AbstractArray{<:Real,2}, normalizationParameters::NTuple{2, AbstractArray{<:Real,2}})
+    dataset .-= normalizationParameters[1];
+    dataset ./= normalizationParameters[2];
+    dataset[:, vec(normalizationParameters[2].==0)] .= 0;
+end
+
+function normalizeZeroMean!(dataset::AbstractArray{<:Real,2})
+    avgs_stds=calculateZeroMeanNormalizationParameters(dataset);
+    normalizeZeroMean!(dataset, avgs_stds);
+end
+
+function normalizeZeroMean( dataset::AbstractArray{<:Real,2}, normalizationParameters::NTuple{2, AbstractArray{<:Real,2}})
+    dataset2=copy(dataset);
+    normalizeZeroMean!(dataset2, normalizationParameters);
+    return dataset2;
+end
+
+function normalizeZeroMean( dataset::AbstractArray{<:Real,2})
+    dataset2=copy(dataset);
+    normalizeZeroMean!(dataset2);
+    return dataset2;
+end
+
+
+function classifyOutputs(outputs::AbstractArray{<:Real,1}; threshold::Real=0.5)
+    return outputs.>=threshold;
+end
+
+function classifyOutputs(outputs::AbstractArray{<:Real,2}; threshold::Real=0.5)
+    if(size(outputs, 2)==1) 
+        vector=classifyOutputs(outputs[:]; threshold=threshold);
+        return reshape(vector, :, 1);
+    else 
+        (_,indicesMaxEachInstance) = findmax(outputs, dims=2); 
+        outputs = falses(size(outputs)); 
+        outputs[indicesMaxEachInstance] .= true; 
+        return outputs;
+    end;
+end
+
+function accuracy(outputs::AbstractArray{Bool,1}, targets::AbstractArray{Bool,1})
+    return mean(targets .== outputs)
+end
+
+function accuracy(outputs::AbstractArray{Bool,2}, targets::AbstractArray{Bool,2})
+    num_cols_targets = size(targets, 2)
+    num_cols_outputs = size(outputs, 2)
+    @assert (num_cols_targets==num_cols_outputs) "las matrices no tienen el mismo numero de columnas"
+    
+    if num_cols_targets == 1
+        return accuracy(targets[:, 1], outputs[:, 1])
+    else
+        return mean(all(targets .== outputs, dims=2))
+    end
+end
+
+function accuracy(outputs::AbstractArray{<:Real,1}, targets::AbstractArray{Bool,1}; threshold::Real=0.5)
+    predicted_classes = outputs .>= threshold
+    return accuracy(predicted_classes, targets)
+end
+
+function accuracy(outputs::AbstractArray{<:Real,2}, targets::AbstractArray{Bool,2}; threshold::Real=0.5)
+    num_cols_targets = size(targets, 2)
+    num_cols_outputs = size(outputs, 2)
+    @assert (num_cols_targets==num_cols_outputs) "las matrices no tienen el mismo numero de columnas"
+    
+    if num_cols_targets == 1
+        return accuracy(outputs, targets)
+    else
+        predicted_classes = classifyOutputs(outputs; threshold=threshold)
+        return accuracy(predicted_classes, targets)    
+    end
+end
+
+
+function buildClassANN(numInputs::Int, topology::AbstractArray{<:Int,1}, numOutputs::Int;
+    transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)))
+    ann = Chain(); 
+    numInputsLayer=numInputs;
+    for i in eachindex(topology)
+        ann = Chain(ann..., Dense(numInputsLayer, topology[i], transferFunctions[i]) );
+        numInputsLayer = topology[i];
+    end; 
+    if (numOutputs>2)
+        ann = Chain(ann..., Dense(numInputsLayer, numOutputs, identity) );
+        ann = Chain(ann..., softmax );
+    else
+        ann = Chain(ann..., Dense(numInputsLayer, 1, σ) );
+    end
+    return ann; 
+end
+
+
+function trainClassANN(topology::AbstractArray{<:Int,1},
+    dataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}};
+    transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)),
+    maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01)
+    inputs = Float32.(dataset[1])
+    targets = dataset[2]
+
+    numInputs = size(inputs, 2)
+    numOutputs = size(targets, 2)
+
+    rna = buildClassANN(numInputs, topology, numOutputs; transferFunctions=transferFunctions)
+    loss(m, x, y) = (size(y, 1) == 1) ? Flux.binarycrossentropy(m(x), y) : Flux.crossentropy(m(x), y)
+
+    opt_state = Flux.setup(Adam(learningRate), rna)
+
+    loss_vector = Float32[]
+    initial_loss = loss(rna, inputs', targets')
+    push!(loss_vector, initial_loss)
+
+    for i in 1:maxEpochs
+        Flux.train!(loss, rna, [(inputs', targets')], opt_state)
+
+        current_loss = loss(rna, inputs', targets')
+        push!(loss_vector, current_loss)
+
+        if current_loss <= minLoss
+            break
+        end
+    end
+
+    return rna, loss_vector
+end
+
+
+function trainClassANN(topology::AbstractArray{<:Int,1},
+    (inputs, targets)::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}};
+    transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)),
+    maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01)
+    targets_matrix = reshape(targets, :, 1)
+
+    return trainClassANN(topology, (inputs, targets_matrix);
+        transferFunctions=transferFunctions,
+        maxEpochs=maxEpochs,
+        minLoss=minLoss,
+        learningRate=learningRate)
+end
+
+
+
+# #SOBREENTRENAMIENTO
+
+function holdOut(N::Int, P::Real)
+    indices = randperm(N);
+    num_test = round(Int, N * (1 - P));
+    training_indices = indices[1:num_test];
+    test_indices = indices[(num_test + 1):N];
+    return (training_indices, test_indices);
+end
+
+function holdOut(N::Int, Pval::Real, Ptest::Real)
+    training_indices, remaining_indices = holdOut(N, Pval + Ptest);
+    val_indices, test_indices = holdOut(length(remaining_indices), Ptest / (Pval + Ptest));
+    val_indices = remaining_indices[val_indices];
+    test_indices = remaining_indices[test_indices];
+    return (training_indices, val_indices, test_indices);
+end
+
+function trainClassANN(topology::AbstractArray{<:Int,1},
+trainingDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}};
+validationDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}=
+(Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0)),
+testDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}=
+(Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0)),
+transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)),
+maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01,
+maxEpochsVal::Int=20)
+    numInputs = size(trainingDataset[1], 2);
+    numOutputs = size(trainingDataset[2], 2);
+    rna=buildClassANN(numInputs, topology, numOutputs; transferFunctions);
+    loss(rna, x,y) = (size(y,1) == 1) ? Flux.binarycrossentropy(rna(x),y) : Flux.crossentropy(rna(x),y);
+    opt_state = Flux.setup(Adam(learningRate), rna);
+
+    
+    training_loss = Float32[];
+    validation_loss = Float32[];
+    test_loss = Float32[];
+
+    
+    best_rna = deepcopy(rna);
+    best_val_loss = Inf;
+
+  
+    train_loss=loss(rna, trainingDataset[1]', trainingDataset[2]');
+    push!(training_loss, train_loss);
+    if !isempty(validationDataset[1]) && !isempty(validationDataset[2])
+        val_loss=loss(rna, validationDataset[1]', validationDataset[2]');
+        push!(validation_loss, val_loss);
+        if val_loss < best_val_loss
+            best_rna = deepcopy(rna);
+            best_val_loss = val_loss;
+            epochs_with_no_better_val_loss = 0
+        else
+            epochs_with_no_better_val_loss += 1
+        end;
+    end;
+    tst_loss = loss(rna, testDataset[1]', testDataset[2]');
+    push!(test_loss, tst_loss);
+
+    for i in 1:maxEpochs
+        Flux.train!(loss, rna, [(trainingDataset[1]', trainingDataset[2]')], opt_state); 
+        train_loss=loss(rna, trainingDataset[1]', trainingDataset[2]');
+        push!(training_loss, train_loss);
+        tst_loss = loss(rna, testDataset[1]', testDataset[2]');
+        push!(test_loss, tst_loss);
+        if !isempty(validationDataset[1]) && !isempty(validationDataset[2])
+            val_loss=loss(rna, validationDataset[1]', validationDataset[2]');
+            push!(validation_loss, val_loss);
+            if val_loss < best_val_loss
+                best_rna = deepcopy(rna);
+                best_val_loss = val_loss;
+                epochs_with_no_better_val_loss = 0
+            else
+                epochs_with_no_better_val_loss += 1
+            end;
+            if epochs_with_no_better_val_loss >= maxEpochsVal
+                break;
+            end;
+        end
+        if train_loss <= minLoss
+            break
+        end
+    end;
+    best_rna = isempty(validationDataset[1]) ? rna : best_rna
+    return best_rna, training_loss, validation_loss, test_loss;
+end
+
+
+function trainClassANN(topology::AbstractArray{<:Int,1},
+trainingDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}};
+validationDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}}=
+(Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0)),
+testDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}}=
+(Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0)),
+transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)),
+maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01,
+maxEpochsVal::Int=20) 
+    training_matrix = reshape(trainingDataset[2], :, 1)
+    validation_matrix = reshape(validationDataset[2], :, 1)
+    test_matrix = reshape(testDataset[2], :, 1)
+    return trainClassANN(topology, (trainingDataset[1], training_matrix); validationDataset=(validationDataset[1], validation_matrix), testDataset=(testDataset[1], test_matrix), transferFunctions, maxEpochs, minLoss, learningRate, maxEpochsVal);
+end
